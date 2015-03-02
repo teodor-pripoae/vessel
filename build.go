9f7bcefdf8fca718401ce83a779f35751c697e63
Teodor Pripoae <teodor.pripoae@gmail.com>
2015-03-02 19:01:19 +0200

    Accept build_image option

--- build.go
+++ build.go
@@ -1,153 +1,157 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "os/exec"
-  "strings"
-  "io/ioutil"
-  "path"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 )
 
 func Build(config Config, app AppConfig) string {
-  checkStdin()
+	checkStdin()
 
-  // this calls slug builder and get container id
-  container_id := buildSlug(config, app)
-  // wait for build to finish, showing build output
-  waitContainer(container_id)
-  // copy slug from container to a temporary location
-  slug_path := copySlug(container_id, config, app)
-  // delete build container
-  deleteContainer(container_id)
+	// this calls slug builder and get container id
+	container_id := buildSlug(config, app)
+	// wait for build to finish, showing build output
+	waitContainer(container_id)
+	// copy slug from container to a temporary location
+	slug_path := copySlug(container_id, config, app)
+	// delete build container
+	deleteContainer(container_id)
 
-  return slug_path
+	return slug_path
 }
 
 func checkStdin() {
-  fi, err := os.Stdin.Stat()
-  if err != nil {
-    panic(err)
-  }
+	fi, err := os.Stdin.Stat()
+	if err != nil {
+		panic(err)
+	}
 
-  if (fi.Mode() & os.ModeCharDevice) != 0 {
-    log.Fatalf("Stdin is empty, please pipe repo content")
-  }
+	if (fi.Mode() & os.ModeCharDevice) != 0 {
+		log.Fatalf("Stdin is empty, please pipe repo content")
+	}
 }
 
-func buildSlug(config Config, app AppConfig) string{
-  args := slugBuilderCmd(config, app)
-    
-  cmd := exec.Command("docker")
-  cmd.Args = args
-  cmd.Stdin = os.Stdin
-  container_id, err := cmd.CombinedOutput()
+func buildSlug(config Config, app AppConfig) string {
+	args := slugBuilderCmd(config, app)
 
-  if err != nil {
-    fmt.Println(string(container_id))
-    log.Fatalf("Error running slugbuilder", err)
-  }
+	cmd := exec.Command("docker")
+	cmd.Args = args
+	cmd.Stdin = os.Stdin
+	container_id, err := cmd.CombinedOutput()
 
-  cid := strings.TrimSpace(string(container_id))
+	if err != nil {
+		fmt.Println(string(container_id))
+		log.Fatalf("Error running slugbuilder", err)
+	}
 
-  fmt.Println("Container id:", cid)
+	cid := strings.TrimSpace(string(container_id))
 
-  return cid
+	fmt.Println("Container id:", cid)
+
+	return cid
 }
 
 func waitContainer(container_id string) {
-  cmd := exec.Command("docker", "logs", "-f", container_id)
-  cmd.Stdout = os.Stdout
-  cmd.Stderr = os.Stderr
-  cmd.Run()
+	cmd := exec.Command("docker", "logs", "-f", container_id)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
 
 // Copy slug.tgz from container to a temporary location
 func copySlug(container_id string, config Config, app AppConfig) string {
-  tmp_dir, err := ioutil.TempDir("", "")
+	tmp_dir, err := ioutil.TempDir("", "")
 
-  if err != nil {
-    log.Fatalf("Error creating TempDir")
-  }
+	if err != nil {
+		log.Fatalf("Error creating TempDir")
+	}
 
-  source := fmt.Sprintf("%s:/tmp/slug.tgz", container_id)
-  output, err := exec.Command("docker", "cp", source, tmp_dir).CombinedOutput()
-  
-  if err != nil {
-    log.Fatalf(string(output))
-  }
+	source := fmt.Sprintf("%s:/tmp/slug.tgz", container_id)
+	output, err := exec.Command("docker", "cp", source, tmp_dir).CombinedOutput()
 
-  slug_path := path.Join(tmp_dir, "slug.tgz")
-  return slug_path
+	if err != nil {
+		log.Fatalf(string(output))
+	}
+
+	slug_path := path.Join(tmp_dir, "slug.tgz")
+	return slug_path
 }
 
 func deleteContainer(container_id string) {
-  cmd := exec.Command("docker", "rm", container_id)
-  cmd.Stdout = os.Stdout
-  cmd.Stderr = os.Stderr
-  cmd.Run()
+	cmd := exec.Command("docker", "rm", container_id)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
 
 func slugBuilderCmd(config Config, app AppConfig) []string {
-  cmd := []string{"docker", "run"}
-  cmd = slugBuilderAttachNetwork(cmd)
-  cmd = slugBuilderAttachStdin(cmd)
-  cmd = slugBuilderAttachEnv(cmd, config, app)
-  cmd = slugBuilderAttachVolumes(cmd, config, app)
-  cmd = slugBuilderAttachImage(cmd, config, app)
+	cmd := []string{"docker", "run"}
+	cmd = slugBuilderAttachNetwork(cmd)
+	cmd = slugBuilderAttachStdin(cmd)
+	cmd = slugBuilderAttachEnv(cmd, config, app)
+	cmd = slugBuilderAttachVolumes(cmd, config, app)
+	cmd = slugBuilderAttachImage(cmd, config, app)
 
-  // fmt.Println(cmd)
-  return cmd
+	// fmt.Println(cmd)
+	return cmd
 }
 
 func slugBuilderAttachNetwork(cmd []string) []string {
-  return append(cmd, "--net='host'")
+	return append(cmd, "--net='host'")
 }
 
 func slugBuilderAttachStdin(cmd []string) []string {
-  // -i -a stdin
-  cmd = append(cmd, "-i")
-  cmd = append(cmd, "-a")
-  return append(cmd, "stdin")
+	// -i -a stdin
+	cmd = append(cmd, "-i")
+	cmd = append(cmd, "-a")
+	return append(cmd, "stdin")
 }
 
-func slugBuilderAttachEnv(cmd []string, config Config, app AppConfig) [] string {
-  // -e BUILDPACK_URL= -e BUILDPACK_VENDOR_URL -e COMMIT_HASH= --env-file=...
-  if app.Build.Env != nil {
-    for _, e := range *app.Build.Env {
-      cmd = append(cmd, "-e")
-      cmd = append(cmd, e)
-    }
-  }
+func slugBuilderAttachEnv(cmd []string, config Config, app AppConfig) []string {
+	// -e BUILDPACK_URL= -e BUILDPACK_VENDOR_URL -e COMMIT_HASH= --env-file=...
+	if app.Build.Env != nil {
+		for _, e := range *app.Build.Env {
+			cmd = append(cmd, "-e")
+			cmd = append(cmd, e)
+		}
+	}
 
-  cmd = append(cmd, "-e")
-  cmd = append(cmd, fmt.Sprintf("BUILDPACK_URL=%s", app.Build.Buildpack))
+	cmd = append(cmd, "-e")
+	cmd = append(cmd, fmt.Sprintf("BUILDPACK_URL=%s", app.Build.Buildpack))
 
-  cmd = append(cmd, "-e")
-  cmd = append(cmd, fmt.Sprintf("BUILDPACK_VENDOR_URL=file://%s", app.Build.BuildpackVendor))
-  
-  cmd = append(cmd, "-e")
-  cmd = append(cmd, fmt.Sprintf("COMMIT_HASH=%s", config.Commit))
+	cmd = append(cmd, "-e")
+	cmd = append(cmd, fmt.Sprintf("BUILDPACK_VENDOR_URL=file://%s", app.Build.BuildpackVendor))
 
-  if app.Build.EnvFile != nil {
-    cmd = append(cmd, fmt.Sprintf("--env-file=%s", *app.Build.EnvFile))  
-  }
+	cmd = append(cmd, "-e")
+	cmd = append(cmd, fmt.Sprintf("COMMIT_HASH=%s", config.Commit))
 
-  return cmd
+	if app.Build.EnvFile != nil {
+		cmd = append(cmd, fmt.Sprintf("--env-file=%s", *app.Build.EnvFile))
+	}
+
+	return cmd
 }
 
-func slugBuilderAttachVolumes(cmd [] string, config Config, app AppConfig) [] string {
-  // -v /etc/buildpacks:/buildpacks -v /var/docker/build/test:/tmp/cache  
-  if app.Build.Volumes != nil {
-    for _, v := range *app.Build.Volumes {
-      cmd = append(cmd, "-v")
-      cmd = append(cmd, v)
-    }
-  }
-  return cmd
+func slugBuilderAttachVolumes(cmd []string, config Config, app AppConfig) []string {
+	// -v /etc/buildpacks:/buildpacks -v /var/docker/build/test:/tmp/cache
+	if app.Build.Volumes != nil {
+		for _, v := range *app.Build.Volumes {
+			cmd = append(cmd, "-v")
+			cmd = append(cmd, v)
+		}
+	}
+	return cmd
 }
 
-func slugBuilderAttachImage(cmd [] string, config Config, app AppConfig) [] string {
-  return append(cmd, "flynn/slugbuilder")
+func slugBuilderAttachImage(cmd []string, config Config, app AppConfig) []string {
+	if app.Build.BuildImage != nil {
+		return append(cmd, *app.Build.BuildImage)
+	} else {
+		return append(cmd, "flynn/slugbuilder")
+	}
 }
--- config.go
+++ config.go
@@ -1,53 +1,54 @@
-package main;
+package main
 
 type Config struct {
-  Config   string
-  Commit   string
-  Deployer string
+	Config   string
+	Commit   string
+	Deployer string
 }
 
 type BuildConfig struct {
-  Buildpack       string       `toml:"buildpack"`
-  BuildpackVendor string       `toml:"buildpack_vendor"`
-  EnvFile         *string      `toml:"env_file"`
-  Env             *[] string   `toml:"env"`
-  Volumes         *[] string   `toml:"volumes"`
+	Buildpack       string    `toml:"buildpack"`
+	BuildpackVendor string    `toml:"buildpack_vendor"`
+	BuildImage      *string   `toml:"build_image"`
+	EnvFile         *string   `toml:"env_file"`
+	Env             *[]string `toml:"env"`
+	Volumes         *[]string `toml:"volumes"`
 }
 
 type DeployConfig struct {
-  SlugLocation  *string      `toml:"slug_location"`
-  Servers       *[] string   `toml:"servers"`
-  Services      *[] string   `toml:"services"`
+	SlugLocation *string   `toml:"slug_location"`
+	Servers      *[]string `toml:"servers"`
+	Services     *[]string `toml:"services"`
 }
 
 type SlackNotifyConfig struct {
-  Team          string    `toml:"team"`
-  Channel       string    `toml:"channel"`
-  Username      string    `toml:"username"`
-  WebhookUrl    string    `toml:"webhook_url"`
-  OnStarted     bool      `toml:"on_started"`
-  OnFailure     bool      `toml:"on_failure"`
-  OnSuccess     bool      `toml:"on_success"`
+	Team       string `toml:"team"`
+	Channel    string `toml:"channel"`
+	Username   string `toml:"username"`
+	WebhookUrl string `toml:"webhook_url"`
+	OnStarted  bool   `toml:"on_started"`
+	OnFailure  bool   `toml:"on_failure"`
+	OnSuccess  bool   `toml:"on_success"`
 }
 
 type OpbeatNotifyConfig struct {
-  OrgId         string    `toml:"org"`
-  AppId         string    `toml:"app"`
-  AppSecret     string    `toml:"token"`
-  OnStarted     bool      `toml:"on_started"`
-  OnFailure     bool      `toml:"on_failure"`
-  OnSuccess     bool      `toml:"on_success"`
+	OrgId     string `toml:"org"`
+	AppId     string `toml:"app"`
+	AppSecret string `toml:"token"`
+	OnStarted bool   `toml:"on_started"`
+	OnFailure bool   `toml:"on_failure"`
+	OnSuccess bool   `toml:"on_success"`
 }
 
 type NotifyConfig struct {
-  Slack      *SlackNotifyConfig
-  Opbeat     *OpbeatNotifyConfig
+	Slack  *SlackNotifyConfig
+	Opbeat *OpbeatNotifyConfig
 }
 
 type AppConfig struct {
-  App      string
-  Stage    string
-  Build    *BuildConfig
-  Deploy   *DeployConfig
-  Notify   *NotifyConfig
+	App    string
+	Stage  string
+	Build  *BuildConfig
+	Deploy *DeployConfig
+	Notify *NotifyConfig
 }
